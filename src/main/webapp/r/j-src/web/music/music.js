0fdbabbbe3405004cde7756631eeb2f4a0c626c8
yli <yli@gemantic.cn>
2013-07-29 15:13:06 +0800

    music dynamic bind

--- src/main/webapp/r/j-src/web/music/music.js
+++ src/main/webapp/r/j-src/web/music/music.js
@@ -48,7 +48,7 @@ $(function () {
     audio.load(first);
 
     // Load in a track on click
-    $('ol li').on("click",function (e) {
+    $('ol').on("click","li",function (e) {
         e.preventDefault();
         $(this).addClass('playing').siblings().removeClass('playing');
         audio.load($('a', this).attr('data-src'));
